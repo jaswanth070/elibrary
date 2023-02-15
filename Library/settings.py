46f3e7c0938742f4e6963a8812735e658b69ed93
Jaswanth Madiya <jaswanthmadiya@gmail.com>
2023-02-15 20:00:59 +0530

    krishna

--- Library/settings.py
+++ Library/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 # import django_heroku
-# import dj_database_url
+import dj_database_url
 # from decouple import config
 # from .info import *
 
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 pip install Django
-python manage.py collectstatic
+# python manage.py collectstatic
 # python manage.py makemigrations
 # python manage.py migrate
